4c729a5c47c80afd63526955906be38621b6288e
litawahyuni <litadwhyni@gmail.com>
2024-11-24 21:42:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+alert('Haiii! Kalian udah klik linknya ya? thank you! :)');
+
+//Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+	navbarNav.classList.toggle('active');
+};
+
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+	if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+		navbarNav.classList.remove('active');
+	}
+});
